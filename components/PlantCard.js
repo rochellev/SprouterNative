8fc2f428fd0bc172653b2baa09553f4fccfab177
rochellev <rochellevroberts@utexas.edu>
2019-09-27 13:48:42 -0700

    test for touching card works, logged the touch

--- components/PlantCard.js
+++ components/PlantCard.js
@@ -5,19 +5,18 @@ import { setRecoveryProps } from 'expo/build/ErrorRecovery/ErrorRecovery';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 
 export default function PlantCard(props) {
+  toggleAlbum = (test) => {
+    console.log(test);
+  }
   return (
-    <TouchableOpacity>
-      
+    <TouchableOpacity onPress={() => this.toggleAlbum("hi")} >
+      <Card title='plant' raised='true' image={props.imageSource}>
+        <Text> {props.plantNote} </Text>
+        <Button
+          icon={<Icon name='add-circle-outline' />}
+          raised={true}
+          style={maxWidth = 1} />
+      </Card>
     </TouchableOpacity>
-    <Card title='plant' raised='true' image={props.imageSource}>
-      <Text>
-        {props.plantNote}
-            </Text>
-      <Button
-        icon={<Icon name='add-circle-outline' />}
-        raised={true}
-        style={maxWidth = 1}
-      />
-    </Card>
   );
 }
