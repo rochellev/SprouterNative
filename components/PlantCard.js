022c5183f8c750fd4a55700d2692d4c7a955dc9e
rochellev <rochellevroberts@utexas.edu>
2019-09-27 15:19:01 -0700

    add more plant cards

--- components/PlantCard.js
+++ components/PlantCard.js
@@ -7,7 +7,6 @@ import { TouchableOpacity } from 'react-native-gesture-handler';
 export default function PlantCard(props) {
   toggleAlbum = (test) => {
     console.log(test);
-
   }
   return (
     <TouchableOpacity onPress={() => this.toggleAlbum("hi")} >
