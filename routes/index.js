12f090d391ea7875dc2760314f98c4934b71c976
rochellev <rochellevroberts@utexas.edu>
2019-10-08 09:59:36 -0700

    add camera route and use enums for route names

--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,23 @@ import React from 'react';
 import {ConnectedRouter} from 'connected-react-router';
 import {NativeRouter, Route} from 'react-router-native';
 import HomeScreen from '../screens/HomeScreen'
-import SettingsScreen from '../screens/SettingsScreen'; 
+import SettingsScreen from '../screens/SettingsScreen';
+import CameraScreen from '../screens/CameraScreen';
 
-const Router = ({history}) => (
+const routes = {
+  HOME : '/',
+  SETTINGS : '/settings',
+  CAMERA : '/camera'
+}
 
+const Router = ({history}) => { 
+return(
     <ConnectedRouter history={history}>
-      <Route path="/" component={HomeScreen} exact={true} />
-      <Route path="/settings" component={SettingsScreen} />
+      <Route path={routes.HOME} component={HomeScreen} exact={true} />
+      <Route path={routes.SETTINGS} component={SettingsScreen} />
+      <Route path={routes.CAMERA} component={CameraScreen} />
     </ConnectedRouter>
-
-);
+)
+};
 
 export default Router;
